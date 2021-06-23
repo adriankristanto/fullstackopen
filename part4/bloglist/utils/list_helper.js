13ed5216330febde26b5ab550e2201dce4376c22
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-06-24 01:45:39 +1000

    added solution to exercises 4.4-4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce(
+        (favorite, blog) => (blog.likes > favorite.likes ? blog : favorite),
+        blogs[0]
+      );
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
