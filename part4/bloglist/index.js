bb5e22a48ed2f219f81986627ed31cf45a72e557
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-06-23 22:40:31 +1000

    added config.js which handles environment variables

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
 const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
+const config = require("./utils/config");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -15,7 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose
   .connect(mongoUrl, {
     useNewUrlParser: true,
@@ -47,7 +47,7 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = config.PORT || 3001;
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
