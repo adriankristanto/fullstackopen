591cc403220d72e4fd5a18a6cd83ff3800b872d8
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-06-23 23:01:35 +1000

    1. moved the main express app to app.js 2. index.js is now only responsible to import the app and start it

---
+++ part4/bloglist/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const bloglistRouter = require("./controllers/bloglist");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) =>
+    logger.error("error connecting to MongoDB:", error.message)
+  );
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", bloglistRouter);
+
+module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,33 +1,11 @@
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const logger = require("./utils/logger");
+const app = require("./app");
 const config = require("./utils/config");
-const bloglistRouter = require("./controllers/bloglist");
+const logger = require("./utils/logger");
 
-const mongoUrl = config.MONGODB_URI;
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) =>
-    logger.error("error connecting to MongoDB:", error.message)
-  );
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", bloglistRouter);
+const server = http.createServer(app);
 
 const PORT = config.PORT || 3001;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
