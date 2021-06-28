b1c7c961918542e4e0b85111afe4bbcd2297b1c2
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-06-28 23:26:01 +1000

    added solution to exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 const bloglistRouter = require("./controllers/bloglist");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 
 const mongoUrl = config.MONGODB_URI;
@@ -27,6 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", bloglistRouter);
 
 app.use(middleware.unknownEndpoint);
