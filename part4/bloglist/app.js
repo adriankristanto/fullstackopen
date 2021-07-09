6935a76d35aa05f6b4844025e78351578a072682
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-07-09 16:08:34 +1000

    added solution to exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,6 +8,7 @@ const config = require("./utils/config");
 const bloglistRouter = require("./controllers/bloglist");
 const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
+const loginRouter = require("./controllers/login");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose
@@ -28,6 +29,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", bloglistRouter);
 
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -20,6 +20,12 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: error.message });
   } else if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).send({ error: "invalid token" });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
   }
 
   next(error);
