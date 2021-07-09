01f9e202d88a1157e68d88368dcca8f963395675
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-07-09 17:36:04 +1000

    added solution to exercise 4.22: moved jwt verification and user extraction to a middleware

--- part4/bloglist/controllers/bloglist.js
+++ part4/bloglist/controllers/bloglist.js
@@ -1,7 +1,6 @@
 const bloglistRouter = require("express").Router();
 const Blog = require("../models/blog");
-const User = require("../models/user");
-const jwt = require("jsonwebtoken");
+const middleware = require("../utils/middleware");
 
 bloglistRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
@@ -12,47 +11,45 @@ bloglistRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
-bloglistRouter.post("/", async (request, response) => {
-  const body = request.body;
+bloglistRouter.post(
+  "/",
+  middleware.userExtractor,
+  async (request, response) => {
+    const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!(request.token && decodedToken.id)) {
-    return response.status(401).json({ error: "token missing or invalid" });
+    const user = request.user;
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user._id,
+    });
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(blog);
+    await user.save();
+    response.status(201).json(savedBlog);
   }
+);
 
-  const user = await User.findById(decodedToken.id);
+bloglistRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    const user = request.user;
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user._id,
-  });
-  const savedBlog = await blog.save();
-  user.blogs = user.blogs.concat(blog);
-  await user.save();
-  response.status(201).json(savedBlog);
-});
+    const blog = await Blog.findById(request.params.id);
 
-bloglistRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!(request.token && decodedToken.id)) {
-    return response.status(401).json({ error: "token missing or invalid" });
+    if (blog.user.toString() === user._id.toString()) {
+      response.status(204).end();
+    } else {
+      response
+        .status(403)
+        .json({ error: "the requested blog is not owned by user" });
+    }
   }
-
-  const user = await User.findById(decodedToken.id);
-
-  const blog = await Blog.findById(request.params.id);
-
-  if (blog.user.toString() === user._id.toString()) {
-    response.status(204).end();
-  } else {
-    response
-      .status(403)
-      .json({ error: "the requested blog is not owned by user" });
-  }
-});
+);
 
 bloglistRouter.put("/:id", async (request, response) => {
   const body = request.body;
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
@@ -15,6 +17,16 @@ const tokenExtractor = (request, response, next) => {
   } else {
     request.token = null;
   }
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!(request.token && decodedToken.id)) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+  request.user = user;
   next();
 };
 
@@ -44,6 +56,7 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   tokenExtractor,
+  userExtractor,
   unknownEndpoint,
   errorHandler,
 };
